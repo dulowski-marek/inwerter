135c0c8b2d9241dbc5fb8cbb075c16d7443c304f
Marek Dulowski <dulowski.marek@gmail.com>
2017-11-26 21:20:57 +0100

    Add optional metadata key param on get method

--- src/metadata/metadata.ts
+++ src/metadata/metadata.ts
@@ -2,8 +2,8 @@ export namespace Metadata {
 
 	export const $key = Symbol.for('inwerter.metadata');
 
-	export function get(target: Object) {
-		return Reflect.getMetadata(Metadata.$key, target);
+	export function get(target: Object, key: symbol | string = Metadata.$key) {
+		return Reflect.getMetadata(key, target);
 	}
 
 	export function set(target: Object, value: any): void {
