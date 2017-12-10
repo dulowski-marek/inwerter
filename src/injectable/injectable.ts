d1fea40d4d653f4650e9b614fb372d8009050768
Marek Dulowski <dulowski.marek@gmail.com>
2017-12-10 19:00:57 +0100

    [Injectable] Define metadata for library symbol for consistency

--- src/injectable/injectable.ts
+++ src/injectable/injectable.ts
@@ -1,5 +1,12 @@
+import { Metadata } from '../metadata';
+
 export function Injectable<T>(): ClassDecorator {
 	return function<T>(target: T): T {
+
+		Metadata.set(target, {
+			args: Metadata.get(target, 'design:paramtypes')
+		});
+
 		return target;
 	}
 }
