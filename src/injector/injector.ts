cccdd85ebb325960098a349f6b44e9bb3f1764b3
Marek Dulowski <dulowski.marek@gmail.com>
2017-12-11 22:41:59 +0100

    Add registry and registered injector containers

--- src/injector/injector.ts
+++ src/injector/injector.ts
@@ -2,4 +2,10 @@ import { Metadata } from '../metadata';
 
 export class Injector {
 
+	private registry: Map<any, any> = new Map();
+	private registered: WeakSet<any> = new WeakSet();
+
+	constructor() {
+
+	}
 }
