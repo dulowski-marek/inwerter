cd75b03229e750f32c6926c0ba11889c3a289f9d
Marek Dulowski <dulowski.marek@gmail.com>
2017-12-11 22:56:19 +0100

    Add resolve method

--- src/injector/injector.ts
+++ src/injector/injector.ts
@@ -16,4 +16,34 @@ export class Injector {
 
 		this.registered.add(provider);
 	}
+
+	public resolve<T>(provider: any): T {
+
+		/**
+		 * Resolve instantly, if has already
+		 * been instantiated. 
+		 */
+		if (this.registry.has(provider)) {
+			return this.registry.get(provider);
+		}
+
+		/**
+		 * Obtain parameter types from provider's 
+		 * metadata annotation.
+		 */
+		const { args }: { args: any[] } = Metadata.get(provider);
+
+		const resolved: T = new provider(args.map(v => this.resolve(v)));
+
+		/**
+		 * Block redundant instantiation by saving
+		 * resolved provider to registry, but only
+		 * if it has been registered beforehand.
+		 */
+		if (this.registered.has(provider)) {
+			this.registry.set(provider, resolved);
+		}
+
+		return resolved;
+	}
 }
