f04070a4af99c176bd241da3aa45bb69dc70f069
Marek Dulowski <dulowski.marek@gmail.com>
2017-12-11 22:38:32 +0100

    [Injector] Class cleanup

--- src/injector/injector.ts
+++ src/injector/injector.ts
@@ -1,37 +1,5 @@
-import { Metadata } from "../metadata/index";
+import { Metadata } from '../metadata';
 
 export class Injector {
 
-	private registry = new Set<Token>();
-	private resolved = new Map<Token, any>();
-
-	constructor() {
-
-	}
-
-	register(token: Token): void {
-		this.registry.add(token);
-	}
-
-	provide<T = any>(token: Token): T {
-		return this.resolved.get(token);
-	}
-
-	resolve<T extends { new(...args: any[]): T }>(target: T): T {
-		let paramtypes = Metadata.get(target, 'design:paramtypes');
-
-		return new target(paramtypes.map(provider => {
-			// If provider has already been resolved
-			if (this.resolved.has(provider)) {
-				return this.resolved.get(provider);
-			}
-
-			let resolved = this.resolve(provider);
-
-			this.resolved.set(provider, resolved);
-			this.registry.delete(provider);
-
-			return resolved;
-		}));
-	}
 }
