c9e1509e37f5ad8cf1050e811b6dc3e5d517fa4f
Marek Dulowski <dulowski.marek@gmail.com>
2017-12-21 14:42:46 +0100

    Add empty array fallback for no reflected design:paramtypes

--- src/provide/provide.ts
+++ src/provide/provide.ts
@@ -2,9 +2,9 @@ import { Metadata } from '../metadata';
 
 export function Provide<T>(target: T): T {
 
-	Metadata.set(target, {
-		args: Metadata.get(target, 'design:paramtypes')
-	});
+	const args = Metadata.get(target, 'design:paramtypes') || [];
+
+	Metadata.set(target, { args });
 
 	return target;
 }
