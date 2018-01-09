c3dc6870296beebfecdc2f8fa08a748dabe50f60
Marek Dulowski <dulowski.marek@gmail.com>
2018-01-09 23:34:11 +0100

    Add Inject parameter decorator

---
+++ src/inject/inject.ts
@@ -0,0 +1,18 @@
+import { Metadata } from '../metadata';
+
+import { merge } from '../helpers/merge';
+
+/**
+ * Allows for specifying arbitrary token,
+ * which should be used to lookup the provider
+ * to be injected.
+ */
+export function Inject(token: any) {
+	return (target: any, key: string, index: number) => {
+		let metadata = Metadata.get(target) || { provide: [] };
+
+		metadata.provide[index] = { ...metadata.provide[index], token };
+
+		Metadata.set(target, metadata);
+	};
+}
