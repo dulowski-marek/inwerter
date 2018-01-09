ca932a3346fc16c7a51f753be725699bfa06fe6a
Marek Dulowski <dulowski.marek@gmail.com>
2018-01-09 23:34:42 +0100

    Make Provide decorator accept previously assigned Metadata

--- src/provide/provide.ts
+++ src/provide/provide.ts
@@ -1,10 +1,12 @@
 import { Metadata } from '../metadata';
 
+import { getParamtypes } from '../helpers/paramtypes';
+import { merge } from '../helpers/merge';
+
 export function Provide<T>(target: T): T {
+	const metadata = Metadata.get(target) || { provide: [] };
 
-	const args = Metadata.get(target, 'design:paramtypes') || [];
-
-	Metadata.set(target, { args });
+	Metadata.set(target, { provide: merge(getParamtypes(target), metadata.provide) });
 
 	return target;
 }
