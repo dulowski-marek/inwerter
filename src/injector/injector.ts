311ba843aef5d216f1c0fd1da7ad520eaea2f2b4
Marek Dulowski <dulowski.marek@gmail.com>
2018-01-09 23:42:22 +0100

    Add _uniformProvideMetadata defaults-returning function

--- src/injector/injector.ts
+++ src/injector/injector.ts
@@ -11,6 +11,48 @@ export interface ResolveRequest {
 	provide: ResolveRequest[]; 
 }
 
+/**
+ * Supplied options, if any, are merged into defaults.
+ */
+export function _uniformProvideMetadata<T>(token: Newable<T> | any, options?: Partial<ProvideMetadata<T>>): ProvideMetadata<T> {
+
+	const defaults: ProvideMetadata<T> = {
+		factory: (...args: any[]) => new token(...args),
+		provide: null,
+		singleton: false
+	};
+
+	if (isClass<T>(token)) {
+
+		defaults.provide = Metadata.get(token).provide;
+
+		return options ? merge(defaults, options) : defaults;
+	}
+
+	const { factory, provide } = options;
+	
+	/**
+	 * Note: token is not a class here.
+	 * 
+	 * If token is not a class and options are not supplied,
+	 * there isn't any appropriate default factory method.
+	 */
+	if (!factory) {
+		throw new Error(`
+			Could not infer how to instantiate provider ${token}.
+			Please supply factory function in options parameter.
+		`);
+	}
+
+	if (!provide) {
+		throw new Error(`
+			Could not infer provide array for ${token}.
+			Please supply it in options parameter.
+		`);
+	}
+
+	return merge(defaults, options);
+}
 
 export class Injector {
 
