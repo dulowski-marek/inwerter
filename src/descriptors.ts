ed0317ab003876c48bf20945fe3a6f83e4ae63ee
Marek Dulowski <dulowski.marek@gmail.com>
2021-10-30 00:21:08 +0200

    Alter build directory structure, execute publish from dist

--- src/descriptors.ts
+++
@@ -1,5 +0,0 @@
-import { ofClass } from './descriptors/ofClass';
-import { ofConst } from './descriptors/ofConst';
-import { ofImplementation } from './descriptors/ofImplementation';
-
-export { ofClass, ofConst, ofImplementation };
---
+++ src/descriptors/index.ts
@@ -0,0 +1,5 @@
+import { ofClass } from './ofClass';
+import { ofConst } from './ofConst';
+import { ofImplementation } from './ofImplementation';
+
+export { ofClass, ofConst, ofImplementation };
